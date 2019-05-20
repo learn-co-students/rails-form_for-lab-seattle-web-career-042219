28746ad637f61778fa2acc8ca1b394339e79070c
Sean Watters <sean@watters.io>
2019-05-20 10:32:51 -0700

    Done.

---
+++ db/migrate/20190520164235_create_school_classes_table.rb
@@ -0,0 +1,8 @@
+class CreateSchoolClassesTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :school_classes_tables do |t|
+      t.string :title
+      t.integer :room_number
+    end
+  end
+end
